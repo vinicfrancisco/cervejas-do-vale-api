b4e4f88e6ce60c66f0ff451cbe5a257b3666dd95
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2021-11-08 22:10:36 -0300

    added alexa skill test

--- src/routes/alexa.ts
+++ src/routes/alexa.ts
@@ -1,18 +1,51 @@
 import { Router, Request, Response } from 'express';
 import { ExpressAdapter } from 'ask-sdk-express-adapter';
-import * as Alexa from 'ask-sdk-core';
+import {
+  RequestHandler,
+  HandlerInput,
+  SkillBuilders,
+  getRequestType,
+  getIntentName,
+} from 'ask-sdk-core';
+import { Response as AlexaResponse } from 'ask-sdk-model';
 
-const skillBuilder = Alexa.SkillBuilders.custom();
+const skillBuilder = SkillBuilders.custom();
 const skill = skillBuilder.create();
 const adapter = new ExpressAdapter(skill, true, true);
 
 const routes = Router();
 
+function buildResponse(
+  speechText: string,
+  shouldEndSession: boolean,
+  cardText: string,
+) {
+  const speechOutput = '<speak>' + speechText + '</speak>';
+  var jsonObj = {
+    version: '1.0',
+    response: {
+      shouldEndSession: shouldEndSession,
+      outputSpeech: {
+        type: 'SSML',
+        ssml: speechOutput,
+      },
+      card: {
+        type: 'Simple',
+        title: 'Cervejas do Vale',
+        content: cardText,
+        text: cardText,
+      },
+    },
+  };
+  return jsonObj;
+}
+
 routes.post(
   '/alexa',
   adapter.getRequestHandlers(),
   (req: Request, res: Response) => {
-    return res.json({ hello: 'world' });
+    // const response =
+    return res.json(buildResponse('Teste', true, 'Teste'));
   },
 );
 
