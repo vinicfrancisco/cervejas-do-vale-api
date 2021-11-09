452cfa3dddea1c1f67fe914f5250819373ca08d8
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2021-11-09 18:24:20 -0300

    test alexa verifier middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import alexaVerifier from 'alexa-verifier-middleware';
 import alexaRoutes from './alexa';
 
 const routes = Router();
+
+alexaRoutes.use(alexaVerifier);
 
 routes.use('/alexa', alexaRoutes);
 
