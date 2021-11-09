67d14947c03a9c716585ccc35d4d1f53e2262bb9
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2021-11-08 21:49:00 -0300

    fixed default port

--- src/server.ts
+++ src/server.ts
@@ -51,4 +51,6 @@ app.use(
   },
 );
 
-app.listen(3333, () => console.log('🚀 Server running on port 3333'));
+app.listen(process.env.PORT || 3333, () =>
+  console.log('🚀 Server running on port 3333'),
+);
