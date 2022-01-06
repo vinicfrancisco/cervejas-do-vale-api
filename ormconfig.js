a43c8b42cd45418e339b874f62dfcc7af8a065aa
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2022-01-06 18:07:43 -0300

    fixed ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,6 @@
+const path = process.env.NODE_ENV === 'prod' ? 'dist' : 'src';
+const type = process.env.NODE_ENV === 'prod' ? 'js' : 'ts';
+
 module.exports = {
   type: "postgres",
   host: process.env.DB_HOST,
@@ -5,10 +8,10 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['src/models/*.ts'],
-  migrations: ["./src/database/migrations/*.ts"],
-  seeds: ["./src/database/seeds/*.ts"],
+  entities: [`${path}/models/*.${type}`],
+  migrations: [`./${path}/database/migrations/*.${type}`],
+  seeds: [`./${path}/database/seeds/*.${type}`],
   cli: {
-    migrationsDir: "./src/database/migrations"
+    migrationsDir: `./${path}/database/migrations`
   }
 };
