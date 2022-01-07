180df86023ee596dc5e98b92f9f1bef10cfa32b8
Vinícius Catafesta Francisco <vinicius.francisco@spartalabs.com.br>
2022-01-07 12:05:19 -0300

    test socket config

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,7 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('https://cervejas-do-vale.herokuapp.com', {
-  transports: ['websocket'],
-});
+const socket = io('https://cervejas-do-vale.herokuapp.com');
 
 export default socket;
